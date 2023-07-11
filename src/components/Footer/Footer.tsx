a7cbee008a6ccc1754e9033b4cf57d1475d9465f
Asura Dev <82215597+minhquantt2002@users.noreply.github.com>
2023-07-11 19:12:51 +0700

    Update Footer.tsx

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,6 +1,5 @@
 import { Container } from './styles'
 
-import linkedinIcon from '../../assets/linkedin.png'
 import githubIcon from '../../assets/github.png'
 import instagramIcon from '../../assets/instagram.png'
 import discordIcon from '../../assets/discord.png'
@@ -12,14 +11,6 @@ export function Footer() {
         <span>Profile</span>
       </a>
       <div className="social-media">
-        {/* <a
-          href="https://www.linkedin.com/in/jtsoares/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <img src={linkedinIcon} alt="Linkedin" />
-        </a> */}
-
         <a
           href="/"
           target="_blank"
